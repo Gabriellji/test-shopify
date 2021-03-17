cf0081ec39ee275639d1690ed053cb7301da391e
Gabriellji <gabriellji@yahoo.com>
2021-03-17 12:16:52 +0300

    feat: server for payment

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth");
 const { verifyRequest } = require("@shopify/koa-shopify-auth");
 const { default: Shopify, ApiVersion } = require("@shopify/shopify-api");
 const Router = require("koa-router");
+const getSubscriptionUrl = require("./server/getSubscriptionUrl");
+
 dotenv.config();
+
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
@@ -16,29 +19,34 @@ Shopify.Context.initialize({
   IS_EMBEDDED_APP: true,
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
 });
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const ACTIVE_SHOPIFY_SHOPS = {};
+
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
   server.keys = [Shopify.Context.API_SECRET_KEY];
+
   server.use(
     createShopifyAuth({
-      afterAuth(ctx) {
-        const { shop, scope } = ctx.state.shopify;
+      async afterAuth(ctx) {
+        const { shop, scope, accessToken } = ctx.state.shopify;
         ACTIVE_SHOPIFY_SHOPS[shop] = scope;
-        ctx.redirect(`/?shop=${shop}`);
+        const returnUrl = `https://${Shopify.Context.HOST_NAME}?shop=${shop}`;
+        const subscriptionUrl = await getSubscriptionUrl(accessToken, shop, returnUrl);
+        ctx.redirect(subscriptionUrl);
       },
-    })
+    }),
   );
 
   router.post("/graphql", verifyRequest(), async (ctx, next) => {
     await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);
   });
-  
+
   const handleRequest = async (ctx) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
