fa4ded35cff170276ae96b590bfd2ecffce19665
Gabriellji <gabriellji@yahoo.com>
2021-03-17 18:50:53 +0300

    feat: route get by idd for product entity

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -14,7 +14,20 @@ router.get("/", async (req, res) => {
   }
 });
 
-
+router.get("/:id", async (req, res) => {
+    try {
+        const product = await Product.findByPk(req.params.id)
+        if (!product) {
+            return res
+                .status(404)
+                .json({ msg: 'Product not found' })
+        }
+        res.json(product)
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
 
 //POST create one
 router.post("/", async (req, res) => {
