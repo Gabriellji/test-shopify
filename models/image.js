263b6e817a50e1516be3a631a716d80764c64a6a
Gabriellji <gabriellji@yahoo.com>
2021-03-19 16:41:01 +0300

    feat: Image model + migration file

---
+++ models/image.js
@@ -0,0 +1,54 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    static associate(models) {
+      Image.belongsTo(models.Product, {
+        foreignKey: "product_id",
+        as: "product",
+      });
+    }
+  }
+  Image.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Product",
+          key: "id",
+        },
+      },
+      position: DataTypes.INTEGER,
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      alt: DataTypes.STRING,
+      width: DataTypes.INTEGER,
+      height: DataTypes.INTEGER,
+      src: DataTypes.STRING,
+      variant_ids: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Variant_ids",
+          key: "id",
+        },
+      },
+      admin_graphql_api_id: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Image",
+    }
+  );
+  return Image;
+};
--- models/variant_ids.js
+++ models/variant_ids.js
@@ -1,30 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Variant_ids extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Variant_ids.belongsTo(models.Image, {
+        foreignKey: "image_id",
+        as: "image",
+      });
     }
-  };
-  Variant_ids.init({
-    id: DataTypes.INTEGER,
-    image_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Image",
-        key: "id"
-      }
+  }
+  Variant_ids.init(
+    {
+      id: DataTypes.INTEGER,
+      image_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Image",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Variant_ids",
     }
-  }, {
-    sequelize,
-    modelName: 'Variant_ids',
-  });
+  );
   return Variant_ids;
 };
